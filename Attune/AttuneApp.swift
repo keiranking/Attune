f8324debbada2f764b998993bb2f6f3cb5018029
Keiran King <me@keiranking.com>
2025-11-30 13:15:21 +0100

    Initial Commit

---
+++ Attune/AttuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttuneApp.swift
+//  Attune
+//
+//  Created by Keiran King on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AttuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
